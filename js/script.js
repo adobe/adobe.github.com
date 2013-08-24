f4931edc75b9afba6f81bb00b4a3e427164ea346
Kim CHOUARD <kim.chouard@gmail.com>
2013-08-23 17:54:51 -0700

    Paralla improve

--- js/script.js
+++ js/script.js
@@ -4,10 +4,8 @@ $(function(){
     $(window).scroll(function () {
         topLogoOriginal = ( $(window).scrollTop()/3 ) - 70;
         $("#brackets").css({ top: topLogoOriginal });
-        topLogoEdge = - ( $(window).scrollTop() * 1.5 ) + 200;
-        $("#brackets_edge").css({ top: topLogoEdge });
-        topLogoInspect = ( $(window).scrollTop() * 1.5 ) - 70;
-        $("#brackets_inspect").css({ top: topLogoInspect });
+        topLogoPlus = - ( $(window).scrollTop() * 1.5 ) + 150;
+        $("#brackets_plus").css({ top: topLogoPlus });
     });
 });
 
