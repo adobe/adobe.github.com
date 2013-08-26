03443b5f486ea468e312a0fbb8778494c0dd25d0
Kim CHOUARD <kim.chouard@gmail.com>
2013-08-26 11:25:44 -0700

    Parallax Org
    > TODO footer
    > TODO template Project & Org

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 $(function(){ 
-    $(document).foundation(); 
+    $(document).foundation({ activeClass: 'hover' }); 
     
     $(window).scroll(function () {
         topLogoOriginal = ( $(window).scrollTop()/3 ) - 70;
-        $("#brackets").css({ top: topLogoOriginal });
-        topLogoPlus = - ( $(window).scrollTop() * 1.5 ) + 150;
-        $("#brackets_plus").css({ top: topLogoPlus });
+        topLogoOriginal_org = ( ( $(window).scrollTop() - $(".header_org").position().top ) / 3 ) - 10 ;
+        console.info(topLogoOriginal_org);
+        topText = - ( $(window).scrollTop() );
+        $("#icon1").css({ top: topLogoOriginal });
+        $("#icon2").css({ top: topLogoOriginal_org });
+        $(".text-header").css({ top: topText });
+        //topLogoPlus = - ( $(window).scrollTop() * 1.5 ) + 150;
+        //$("#brackets_plus").css({ top: topLogoPlus });
     });
 });
 
