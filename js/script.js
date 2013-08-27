45e8f2efc4e9eba831b35ecd0ecf2a1fe9f52c6c
Kim CHOUARD <kim.chouard@gmail.com>
2013-08-26 17:21:16 -0700

    Auto pull repos + display with angular
    > Direct call to api.github.com to transform it into angular resource
    and display it with ng-repeat.

--- js/script.js
+++ js/script.js
@@ -1,16 +1,58 @@
+/* ----------------------------------------------------------------------------
+                GitHup Ctrl
+---------------------------------------------------------------------------- */
+
+//Initialisation
+var app = angular.module("AdobeOpenSource", ["ngResource"])
+
+//Get Adobe Github repos
+app.factory("GitAdobe", function($resource) {
+    return $resource("https://api.github.com/users/adobe/:type?sort=updated", {type: "@type"})
+});
+
+//Get Adobe Github repos
+app.factory("GitRepo", function($resource) {
+    return $resource("https://api.github.com/repos/adobe/:repo/:info", {owner: "@owner", languages: "@languages"})
+});
+
+this.ProjectCtrl = function($scope, GitAdobe, GitRepo) {
+    //Init display range
+    $scope.projFirst = 0;
+    $scope.projLast = 10;
+    
+    $scope.projects = GitAdobe.query({ type: "repos" }, function() {
+        console.info( $scope.projects );
+    });
+    $scope.test = GitRepo.query({ repo: "brackets", info: "languages" }, function() {
+        console.info( $scope.test );
+    });
+};
+
+/* ----------------------------------------------------------------------------
+                Parrallax Scrolling
+---------------------------------------------------------------------------- */
+
+var scrollTop, topLogo_footer, topLogo_org, topLogo_header;
 $(function(){ 
-    $(document).foundation({ activeClass: 'hover' }); 
+    $(document).foundation();
+    
     
     $(window).scroll(function () {
-        topLogoOriginal = ( $(window).scrollTop()/3 ) - 70;
-        topLogoOriginal_org = ( ( $(window).scrollTop() - $(".header_org").position().top + 350 ) / 2 ) - 10 ;
-        console.info(topLogoOriginal_org);
-        topText = - ( $(window).scrollTop() );
-        $("#icon1").css({ top: topLogoOriginal });
-        $("#icon2").css({ top: topLogoOriginal_org });
-        $(".text-header").css({ top: topText });
-        //topLogoPlus = - ( $(window).scrollTop() * 1.5 ) + 150;
-        //$("#brackets_plus").css({ top: topLogoPlus });
+        scrollTop = $(window).scrollTop();
+        
+        if (scrollTop < ($(".header").height() + 20) ) {
+            topLogo_header = ( $(window).scrollTop()/3 ) - 70;
+            $("#logo1").css({ top: topLogo_header });
+            $("#text-header").css({ top: - scrollTop });
+        }
+        
+        topLogo_org = ( ( $(window).scrollTop() - $("#header_org").position().top + 350 ) / 2 ) - 10 ;
+        if ( topLogo_org > 90 )
+            topLogo_org = 90;
+        $("#logo2").css({ top: topLogo_org });
+        
+        topLogo_footer = ( scrollTop + $(window).height() - $("#footer").position().top - $("#footer").height()*2 ) - 65;
+        $("#logo3").css({ bottom: topLogo_footer });
     });
 });
 
