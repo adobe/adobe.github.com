f35105e34cf1a5797f83729f5bb071c84c4e6464
Kim CHOUARD <kim.chouard@gmail.com>
2013-08-26 17:56:21 -0700

    Auto hide logo adobe
    > Work in progress: Languages of each repository

--- js/script.js
+++ js/script.js
@@ -20,7 +20,14 @@ this.ProjectCtrl = function($scope, GitAdobe, GitRepo) {
     $scope.projFirst = 0;
     $scope.projLast = 10;
     
+    //Get all projects
     $scope.projects = GitAdobe.query({ type: "repos" }, function() {
+        $($scope.projects).each(function(i) {
+            this.languages = GitRepo.query({ repo: this.name, info: "languages" }, function() {
+                console.info( $scope.projects[i].languages );
+            });
+        });
+        
         console.info( $scope.projects );
     });
     $scope.test = GitRepo.query({ repo: "brackets", info: "languages" }, function() {
@@ -40,17 +47,35 @@ $(function(){
     $(window).scroll(function () {
         scrollTop = $(window).scrollTop();
         
+        //Logo animation
+        var baseH = 100;
+        var adobeLogoLimit = $(".header").height() - 150;
+        if ( scrollTop > adobeLogoLimit ) {
+            if ( (scrollTop - adobeLogoLimit) <= 50 ) {
+                newHeight = baseH - (scrollTop - adobeLogoLimit)
+                $(".wrapLogoTop").css({height: newHeight });
+            } else {
+                $(".wrapLogoTop").css({height: baseH-50 });
+            };
+        }
+        else if (scrollTop < adobeLogoLimit) {
+            $(".wrapLogoTop").css({height: baseH });
+        }
+        
+        //First parallax: header
         if (scrollTop < ($(".header").height() + 20) ) {
             topLogo_header = ( $(window).scrollTop()/3 ) - 70;
             $("#logo1").css({ top: topLogo_header });
             $("#text-header").css({ top: - scrollTop });
         }
         
+        //2nd parrallax: organisations
         topLogo_org = ( ( $(window).scrollTop() - $("#header_org").position().top + 350 ) / 2 ) - 10 ;
         if ( topLogo_org > 90 )
             topLogo_org = 90;
         $("#logo2").css({ top: topLogo_org });
         
+        //3rd parrallax: footer
         topLogo_footer = ( scrollTop + $(window).height() - $("#footer").position().top - $("#footer").height()*2 ) - 65;
         $("#logo3").css({ bottom: topLogo_footer });
     });
