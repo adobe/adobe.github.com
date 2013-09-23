be780b604d43376c9c27cb8a3ef7b8fec5aa9ecf
Kim CHOUARD <kim.chouard@gmail.com>
2013-09-23 23:40:11 +0200

    Offline system for repository
    TODO: make automatic updates of repo.json

--- js/script.js
+++ js/script.js
@@ -3,23 +3,26 @@
 ---------------------------------------------------------------------------- */
 
 //Initialisation
-var app = angular.module("AdobeOpenSource", ["ngResource"])
+var app = angular.module("AdobeOpenSource", ["ngResource"]);
 
+
+/* ----------------------------------------------------------------------------
+                Filters */
 app.filter('star', function() {
 	return function(infos) {
         switch(infos.id)
         {
         case 0:
-          state = (infos.value > 1) ? "on" : "off";
-          break;
-        case 1:
           state = (infos.value > 5) ? "on" : "off";
           break;
-        case 2:
+        case 1:
           state = (infos.value > 30) ? "on" : "off";
           break;
-        case 3:
+        case 2:
           state = (infos.value > 100) ? "on" : "off";
+          break;
+        case 3:
+          state = (infos.value > 500) ? "on" : "off";
           break;
         case 4:
           state = (infos.value > 1000) ? "on" : "off";
@@ -57,7 +60,7 @@ app.filter('niceNum', function() {
 
 			niceNum =  rest + "'" + niceNum;
 			num = Math.floor(num / 1000);
-        }
+    }
         
 		return (niceNum == "") ? "0" : niceNum.substring(0, niceNum.length-1);
     }
@@ -70,63 +73,170 @@ app.filter('shortenStr', function() {
     }
 });
 
-//Get Adobe Github repos
-app.factory("GitAdobe", function($resource) {
-    return $resource("https://api.github.com/users/adobe/:type?sort=updated", {type: "@type"})
+app.filter('timeDiff', function() {
+	return function (timeRaw) {
+    var now = new Date();
+    var time = new Date( timeRaw );
+		var diff = now - time;
+		
+		var days = Math.floor(diff / 1000 / 60 / (60 * 24));
+
+    var date_diff = new Date( diff );
+		
+		var sDate = "";
+    if (days != 0) {
+			//Count and display nb years
+			var years = Math.floor( days / 365 );
+			if (years != 0) {
+				sDate += years + " year";
+				sDate += (years > 1) ? "s " : " ";
+				//Reduce it to the number of days not counted
+				days = days % 365;
+			}
+			
+			var months = Math.floor( days / 30 );
+			if (months != 0 ) {
+				sDate += months + " month";
+				sDate += (months > 1) ? "s " : " ";
+			}
+			else if (years == 0) {
+				sDate += days + " d "+ date_diff.getHours() + " h";
+			}
+		}
+		else {
+			sDate += date_diff.getHours() + " h " + date_diff.getMinutes() + " min";
+		}
+		
+		return sDate;
+  }
+});
+
+/* ----------------------------------------------------------------------------
+                Main Controller */
+
+//Get Adobe Github orgs
+app.factory("OrgsAdobe", function($resource) {
+    return $resource("/offline/orgs.json")
+});
+
+//Get Adobe Github orgs
+app.factory("ReposAdobe", function($resource) {
+    return $resource("/offline/repos.json")
 });
 
 //TODO : Manage offline project list when errors
-this.ProjectCtrl = function($scope, GitAdobe) {
-    //Be able to call math functions
-    $scope.Math = Math;
-    
-    //Init display range
-    $scope.projFirst = 0;
-    $scope.projLast = 10;
-    
-    //Get all projects
-    $scope.projects = GitAdobe.query({ type: "repos" }, function() {
-        $($scope.projects).each(function(i) {
-            var actProj = $scope.projects[i];
-            $scope.projects.languages = [ $scope.projects.language ];
-            
-            var request = $.ajax({
-                url: actProj.languages_url,
-                cache: true
-            });
-            
-            request.done( function( repLang ) {
-                $scope.$apply( function () {
-                    actProj.languages = new Array();
-                    actProj.languagesTotal = 0;
-                    for (var key in repLang) {
-                        actProj.languages.push( { name: key, value: repLang[key] } );
-                        actProj.languagesTotal += repLang[key];
-                    }
-                });
-            });
-            
-            //If it fails, we use the main language only
-            request.fail( function ( jqXHR, textStatus ) {
-                $scope.$apply( function () {
-                    $scope.projects.languages = $scope.projects.language;
-                });
-                console.log("Language query failed for "+actProj.name+", error: "+textStatus, jqXHR);
-            });
-        });
-        
-        console.info($scope.projects);
-    });
-    
-    $scope.showHideProj = function() {
-        if ($scope.projLast == 10) {
-            $scope.projLast = 100
-        }
-        else {
-            $("html, body").animate({ scrollTop: 350 }, 100);
-            $scope.projLast = 10
-        }
-    }
+this.GitHubCtrl = function($scope, $filter, ReposAdobe, OrgsAdobe) {
+	//Be able to call math functions
+	$scope.Math = Math;
+	
+	//Loading active
+	$scope.loading = true;
+	
+	//Init display range
+	$scope.projFirst = 0;
+	$scope.projLast = 10;
+	
+	//Reference Orgs
+	$scope.orgs = OrgsAdobe.query( function() { });
+	
+	//Reference Orgs
+	$scope.projects = ReposAdobe.query(function() { 
+		//Loading over
+		$scope.loading = false;
+		console.info($scope.projects);
+	});
+	
+	$scope.showHideProj = function() {
+		if ($scope.projLast == 10) {
+			$scope.projLast = 100;
+		}
+		else {
+			$("html, body").animate({ scrollTop: 350 }, 100);
+			$scope.projLast = 10
+		}
+	}
+};
+
+/* ----------------------------------------------------------------------------
+                Offline Controller */
+
+//Get Adobe Github repos
+app.factory("GitAdobe", function($resource) {
+    return $resource("https://api.github.com/users/:user/:type?sort=updated", {user: "@user", type: "@type"})
+});
+
+//TODO : Manage offline project list when errors
+this.OfflineCtrl = function($scope, $filter, GitAdobe, OrgsAdobe) {	
+	$scope.getOffline = function() {
+		//Initialize the project array
+		$scope.projects = new Array();
+	
+		//Loading active
+		$scope.loading = true;
+		
+		//Reference Orgs
+		$scope.orgs = OrgsAdobe.query(function() {
+			//To detect when all orgs has been processed
+			var recievedOrgProj = 0;
+			
+			//Get all projects from each org
+			$($scope.orgs).each(function (i) {
+				
+				GitAdobe.query({ user: $scope.orgs[i].userName, type: "repos" }, function(tempProjects) {
+					recievedOrgProj++;
+					
+					$scope.tempProjects = tempProjects;
+					
+					$(tempProjects).each(function(iProj) {
+						//And push it to the list
+						$scope.projects.push({
+							name: tempProjects[iProj].name
+							, watchers_count: tempProjects[iProj].watchers_count
+							, org: $scope.orgs[i].name
+							, languages: [ tempProjects[iProj].language ]
+							, description: tempProjects[iProj].description
+							, pushed_at: tempProjects[iProj].pushed_at
+							, html_url: tempProjects[iProj].html_url
+							, homepage: tempProjects[iProj].homepage
+						});
+						
+						//Then query for more languages
+						var actProj = $scope.projects[$scope.projects.length-1];
+						
+						var request = $.ajax({
+							url: tempProjects[iProj].languages_url,
+							cache: true
+						});
+						
+						request.done( function( repLang ) {
+							$scope.$apply( function () {
+								actProj.languages = new Array();
+								actProj.languagesTotal = 0;
+								for (var key in repLang) {
+									actProj.languages.push( { name: key, value: repLang[key] } );
+									actProj.languagesTotal += repLang[key];
+								};
+							});
+						});
+						
+						//If it fails, we use the main language only
+						request.fail( function ( jqXHR, textStatus ) {
+							console.log("Language query failed for "+actProj.name+", error: "+textStatus, jqXHR);
+						});
+					});
+					
+					if (recievedOrgProj == $scope.orgs.length) {
+						$scope.projects = $filter('orderBy')($scope.projects, '-watchers_count');
+						
+						//Everything loaded!
+						$scope.loading = false;
+						//console.info($scope.projects);
+					}
+				});
+				
+			});
+		});
+	}
 };
 
 
@@ -137,7 +247,36 @@ this.ProjectCtrl = function($scope, GitAdobe) {
 var scrollTop, topLogo_footer, topLogo_org, topLogo_header;
 $(function(){ 
     $(document).foundation();
-    
+    $(document).foundation('orbit', {
+      animation: 'fade',
+      timer_speed: 10000,
+      pause_on_hover: true,
+      resume_on_mouseout: false,
+      animation_speed: 500,
+      stack_on_small: true,
+      navigation_arrows: false,
+      slide_number: false,
+      container_class: 'orbit-container',
+      stack_on_small_class: 'orbit-stack-on-small',
+      next_class: 'orbit-next',
+      prev_class: 'orbit-prev',
+      timer_container_class: 'orbit-timer',
+      timer_paused_class: 'paused',
+      timer_progress_class: 'orbit-progress',
+      slides_container_class: 'orbit-slides-container',
+      bullets_container_class: 'orbit-bullets',
+      bullets_active_class: 'active',
+      slide_number_class: 'orbit-slide-number',
+      caption_class: 'orbit-caption',
+      active_slide_class: 'active',
+      orbit_transition_class: 'orbit-transitioning',
+      bullets: true,
+      timer: false,
+      variable_height: false,
+      before_slide_change: function(){},
+      after_slide_change: function(){}
+    });
+        
     
     $(window).scroll(function () {
         scrollTop = $(window).scrollTop();
