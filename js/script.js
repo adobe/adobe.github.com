a45cd8bc122d38016face228dbc4b34a7f3aed33
Kim CHOUARD <kim.chouard@gmail.com>
2013-12-30 17:49:15 +0100

    no parallax on mobile

--- js/script.js
+++ js/script.js
@@ -213,7 +213,6 @@ this.GitHubCtrl = function($scope, $filter, ReposAdobe, FeaturedHeader) {
     
     // hFeatured on the header
     $scope.featureds = FeaturedHeader.query( function() {
-		console.log("YEa");
 		$scope.featureds = $scope.featureds[0];
 		var keys = Object.keys($scope.featureds);
         for (var i = 0; i < keys.length; i++) {
@@ -298,37 +297,64 @@ this.GitHubCtrl = function($scope, $filter, ReposAdobe, FeaturedHeader) {
 
 
 /* ----------------------------------------------------------------------------
+                Mobile Detection
+---------------------------------------------------------------------------- */
+
+var isMobile = function(a) {
+	var test1 = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a);
+	var test2 = /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4));
+	if(test1 || test2) {
+		return true;
+	} else {
+		return false;
+	}
+};
+
+
+/* ----------------------------------------------------------------------------
                 Parrallax Scrolling
+---------------------------------------------------------------------------- */
+
+var scrollUpdate = function () {
+	scrollTop = $(window).scrollTop();
+		
+	// ----------------------------------------------------------------------------
+	//					First parallax: header
+	if (scrollTop < ($("#featuredProj").height() + 20) ) {
+		topLogo_header = ( $(window).scrollTop()/3 ) - 70;
+		$("#featuredProj .logo").css({ top: topLogo_header });
+		$("#featuredProj .text-header").css({ bottom: 0 + scrollTop });
+	}
+	
+	// ----------------------------------------------------------------------------
+	//					2nd parrallax: organisations
+	topLogo_org = ( ( $(window).scrollTop() - $("#featuredOrg").position().top + 350 ) / 2 ) - 10 ;
+	if ( topLogo_org > 90 )
+		topLogo_org = 90;
+	$("#featuredOrg .logo").css({ top: topLogo_org });
+	
+	// ----------------------------------------------------------------------------
+	//					3rd parrallax: footer
+	topLogo_footer = ( scrollTop + $(window).height() - $("#footer").position().top - $("#footer").height()*2 ) - 115;
+	$("#logo3").css({ bottom: topLogo_footer });
+}
+
+
+/* ----------------------------------------------------------------------------
+                Main
 ---------------------------------------------------------------------------- */
 
 var scrollTop, topLogo_footer, topLogo_org, topLogo_header;
 $(function(){ 
     $(document).foundation();
         
-    
-    $(window).scroll(function () {
-        scrollTop = $(window).scrollTop();
-			
-        // ----------------------------------------------------------------------------
-        //					First parallax: header
-        if (scrollTop < ($("#featuredProj").height() + 20) ) {
-            topLogo_header = ( $(window).scrollTop()/3 ) - 70;
-            $("#featuredProj .logo").css({ top: topLogo_header });
-            $("#featuredProj .text-header").css({ bottom: 0 + scrollTop });
-        }
-        
-        // ----------------------------------------------------------------------------
-        //					2nd parrallax: organisations
-        topLogo_org = ( ( $(window).scrollTop() - $("#featuredOrg").position().top + 350 ) / 2 ) - 10 ;
-        if ( topLogo_org > 90 )
-            topLogo_org = 90;
-        $("#featuredOrg .logo").css({ top: topLogo_org });
-        
-        // ----------------------------------------------------------------------------
-        //					3rd parrallax: footer
-        topLogo_footer = ( scrollTop + $(window).height() - $("#footer").position().top - $("#footer").height()*2 ) - 115;
-        $("#logo3").css({ bottom: topLogo_footer });
-    });
+    var mobile = isMobile(navigator.userAgent||navigator.vendor||window.opera);
+	
+	if (mobile) {
+		$(window).off("scroll", scrollUpdate);
+	} else {
+		$(window).on("scroll", scrollUpdate);
+	}
 });
 
 $(document).foundation();
